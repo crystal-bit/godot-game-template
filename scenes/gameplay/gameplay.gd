736c772940b9ddfaa013aea3b2d4dfdf43ecb7cb
Davide Cristini <davcri91@gmail.com>
2020-10-26 09:39:31 +0100

    Small refactor.
    Remove unused Label script
    Create separate scene for PauseLayer

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 
 func pre_start(params):
-	print("\npre_start called with params = ")
+	print("")
+	print("pre_start called with params = ")
 	for key in params:
 		var val = params[key]
 		printt("", key, val)
 
 
 func start():
-	print("\nStart game logic!")
+	print("")
+	print("Start game logic!")
