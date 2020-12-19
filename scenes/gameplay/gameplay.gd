372b9bcf6b5ceea29b799f0ec1e15a0c8683aba4
Davide Cristini <davcri91@gmail.com>
2020-12-19 12:27:35 +0100

    Clean white spaces

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,22 +1,22 @@
 extends Node2D
 
 
-# `pre_start()` is called when a scene is totally loaded. 
+# `pre_start()` is called when a scene is totally loaded.
 # Use this function to receive params from the scene which
-# called `Game.change_scene(params)` and to init your 
-# new scene. 
+# called `Game.change_scene(params)` and to init your
+# new scene.
 #
 # At this point the game is paused (`get_tree().paused = true`).
 func pre_start(params):
-	print("\n gameplay.gd:pre_start() called with params = ")
+	print("\ngameplay.gd:pre_start() called with params = ")
 	for key in params:
 		var val = params[key]
 		printt("", key, val)
 
 
-# `start()` is called when the graphic transition ends. 
+# `start()` is called when the graphic transition ends.
 func start():
-	print("\n gameplay.gd:start() called")
+	print("\ngameplay.gd:start() called")
 	var active_scene: Node = Game.get_active_scene()
 	print("\nCurrent active scene is: ",
 		active_scene.name, " (", active_scene.filename, ")")
