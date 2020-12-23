16e2f043bf0c3b3bda8746e60db5c87b8253afd5
Davide <davcri91@gmail.com>
2020-12-23 22:19:57 +0100

    pause-layer.gd: small refactor

--- scenes/gameplay/pause-layer/pause-layer.gd
+++ scenes/gameplay/pause-layer/pause-layer.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-onready var pause_screen := $Pause
+onready var pause := $Pause
 onready var resume_option := $Pause/VBoxOptions/Resume
 
 
@@ -15,13 +15,13 @@ func _unhandled_input(event):
 
 func resume():
 	get_tree().paused = false
-	pause_screen.hide()
+	pause.hide()
 
 
 func pause_game():
 	resume_option.grab_focus()
 	get_tree().paused = true
-	pause_screen.show()
+	pause.show()
 
 
 func _on_Resume_pressed():
@@ -33,7 +33,3 @@ func _on_Main_Menu_pressed():
 	multiple click during animations.
 	For other parts of the game the pause state takes care of this."""
 	Game.change_scene("res://scenes/menu/menu.tscn")
-	# if, by accident, you try to load the main scene (the container
-	# for this template) it prevents to load it because
-	# it's in the Game.scenes_denylist
-	# Game.change_scene("res://scenes/main.tscn")
