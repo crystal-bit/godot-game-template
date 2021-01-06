45d09cb4bbb0401bd9b0755aa2a0d03f82403d46
Davide Cristini <davcri91@gmail.com>
2021-01-06 17:51:49 +0100

    Add Game.size property.
    Exposes main.size which contains the current viewport size

--- autoload/game.gd
+++ autoload/game.gd
@@ -6,8 +6,9 @@
 extends Node
 
 
-onready var main: Main = get_node_or_null("/root/Main")
+var size: Vector2 setget , get_size
 
+onready var main: Main = get_node_or_null("/root/Main")
 
 func _ready():
 	if main == null:
@@ -44,3 +45,7 @@ func reparent_node(node: Node2D, new_parent, update_transform = true):
 
 func get_active_scene() -> Node:
 	return main.get_active_scene()
+
+
+func get_size():
+	return main.size
