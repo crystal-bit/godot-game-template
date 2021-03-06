1c950230c817a239e527f04f9052a0d0c05dc5af
Davide Cristini <davcri91@gmail.com>
2021-03-06 15:45:11 +0100

    Play scene: fix crash.
    Closes #32

--- autoload/game.gd
+++ autoload/game.gd
@@ -10,6 +10,7 @@ var size: Vector2 setget , get_size
 
 onready var main: Main = get_node_or_null("/root/Main")
 
+
 func _ready():
 	if main == null:
 		call_deferred("_force_main_scene_load")
@@ -23,7 +24,7 @@ func _force_main_scene_load():
 	var played_scene = get_tree().current_scene
 	var root = get_node("/root")
 	main = load("res://scenes/main/main.tscn").instance()
-	main.initial_fade_active = false
+	main.splash_transition_on_start = false
 	root.remove_child(played_scene)
 	root.add_child(main)
 	main.active_scene_container.get_child(0).queue_free()
