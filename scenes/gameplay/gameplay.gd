7c3c38e5a7db44345d3c6bdc1df6a7cb2d98d5d9
Davide Cristini <davcri91@gmail.com>
2021-03-22 13:59:46 +0100

    Gameplay scene:  use set_process to show `pre_start` and `start` callbacks.
    Update doc

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -2,18 +2,15 @@ extends Node2D
 
 var elapsed = 0
 
-# `pre_start()` is called when a scene is totally loaded.
-# Use this function to receive params from the scene which
-# called `Game.change_scene(params)` and to init your
-# new scene.
-#
-# At this point the game is paused (`get_tree().paused = true`).
+# `pre_start()` is called when a scene is loaded.
+# Use this function to receive params from `Game.change_scene(params)`.
 func pre_start(params):
 	print("\ngameplay.gd:pre_start() called with params = ")
 	for key in params:
 		var val = params[key]
 		printt("", key, val)
 	$Sprite.position = Game.size / 2
+	set_process(false)
 
 
 # `start()` is called when the graphic transition ends.
@@ -22,10 +19,10 @@ func start():
 	var active_scene: Node = Game.get_active_scene()
 	print("\nCurrent active scene is: ",
 		active_scene.name, " (", active_scene.filename, ")")
+	set_process(true)
 
 
 func _process(delta):
 	elapsed += delta
 	$Sprite.position.x = Game.size.x / 2 + 150 * sin(2 * 0.4 * PI * elapsed)
 	$Sprite.position.y = Game.size.y / 2 + 100 * sin(2 * 0.2 *  PI * elapsed)
-
