715bdda88f793bfb8cb801cb7ca381a0cf16cd36
Davide Cristini <davcri91@gmail.com>
2022-04-13 14:46:45 +0200

    scenes.gd: fix "instanced_scn.pre_start()" handling (fixes #66)

--- addons/game-template/scenes.gd
+++ addons/game-template/scenes.gd
@@ -62,7 +62,10 @@ func _set_new_scene(resource: PackedScene):
 	get_tree().root.add_child(instanced_scn) # triggers _ready
 	get_tree().current_scene = instanced_scn
 	if instanced_scn.has_method("pre_start"):
-		yield(instanced_scn.pre_start(_params), "completed")
+		var coroutine_state = instanced_scn.pre_start(_params)
+		if (coroutine_state is GDScriptFunctionState) and (coroutine_state.is_valid()):
+			yield(coroutine_state, "completed")
+	# else... this is a normal function that just ended
 	if transitions:
 		transitions.fade_out()
 	if transitions:
