74b5a674ff6fcfdbf777c0a66202c9be5c82fa21
Davide Cristini <davcri91@gmail.com>
2025-06-07 23:03:07 +0200

    fix: make resource loader work on web exports.
    Fixes #102

--- addons/ggt-core/utils/resource_multithread_loader.gd
+++ addons/ggt-core/utils/resource_multithread_loader.gd
@@ -8,51 +8,57 @@ const SIMULATED_DELAY_MS = 32
 var thread
 var stages_amount: int
 
-
 func load_resource(path):
-	if thread == null:
-		thread = Thread.new()
-	if ResourceLoader.has_cached(path):
-		return ResourceLoader.load(path)
+	if OS.has_feature("web"):
+		_handle_load(path, true)
 	else:
-		var state = thread.start(Callable(self, "_thread_load").bind(path))
-		if state != OK:
-			push_error("Error while starting thread: " + str(state))
+		if thread == null:
+			thread = Thread.new()
+		if ResourceLoader.has_cached(path):
+			return ResourceLoader.load(path)
+		else:
+			_load_resource_threaded(path)
 
 
-func _thread_load(path):
+func _load_resource_threaded(path):
+	var state = thread.start(Callable(self, "_handle_load").bind(path, false))
+	if state != OK:
+		push_error("Error while starting thread: " + str(state))
+
+
+func _handle_load(path, runs_on_main_thread: bool):
 	var status = ResourceLoader.load_threaded_request(path)
 	if status != OK:
-		push_error(status, "threaded resource failed")
+		push_error(status, "resource request failed")
 		return
+
 	var res = null
 	var progress_arr = []
 
 	while true:
 		var loading_status = ResourceLoader.load_threaded_get_status(path, progress_arr)
+
 		if loading_status == ResourceLoader.THREAD_LOAD_LOADED:
 			call_deferred("emit_signal", "resource_stage_loaded", float(progress_arr[0]))
 			res = ResourceLoader.load_threaded_get(path)
 			break
 		elif loading_status == ResourceLoader.THREAD_LOAD_FAILED:
-			push_error("Thread load failed for: {0}".format([path]))
-			break
+			push_error("Load failed for: {0}".format([path]))
+			return
 		elif loading_status == ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
-			push_error("Thread invalid resource: {0}".format([path]))
+			push_error("Invalid resource: {0}".format([path]))
+			return
+
+		if runs_on_main_thread:
+			await Engine.get_main_loop().create_timer(SIMULATED_DELAY_MS / 1000.0).timeout
 		else:
-			call_deferred("emit_signal", "resource_stage_loaded", float(progress_arr[0]))
-			pass
-		OS.delay_msec(SIMULATED_DELAY_MS)
+			OS.delay_msec(SIMULATED_DELAY_MS)
 	call_deferred("_thread_done", res)
 
 
 func _thread_done(resource):
 	assert(resource)
-	# Always wait for threads to finish, this is required on Windows.
-	thread.wait_to_finish()
+	if thread:
+		# Always wait for threads to finish, this is required on Windows.
+		thread.wait_to_finish()
 	emit_signal("resource_loaded", resource)
-
-
-#func _exit_tree():
-#	if thread and thread.is_alive():
-#		thread.wait_to_finish()
