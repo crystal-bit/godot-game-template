a18a7d874e94e20dd286367cf52f7efb408daa71
Davide Cristini <davcri91@gmail.com>
2025-06-21 11:51:03 +0200

    refactor: cleanup debug_shortucts.
    - include all shortcuts in debug_shortcuts.gd
    - remove fullscreen shortcut
    - update version in plugin.cfg

--- addons/ggt-debug-shortcuts/autoload/debug_shortcuts.gd
+++ addons/ggt-debug-shortcuts/autoload/debug_shortcuts.gd
@@ -4,3 +4,18 @@ extends Node
 func _ready():
 	if OS.is_debug_build() == false:
 		queue_free()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("debug_pause_game"):
+			get_tree().paused = !get_tree().paused
+		elif event.is_action_pressed("debug_quit_game"):
+			get_tree().quit()
+		elif event.is_action_pressed("debug_restart_scene"):
+			var ggt_game_autoload = get_node_or_null("/root/Game")
+			# if "ggt-core" addon is enabled
+			if ggt_game_autoload:
+				ggt_game_autoload.restart_scene()
+			else:
+				get_tree().reload_current_scene()
--- addons/ggt-debug-shortcuts/ggt-debug-shortcuts.gd
+++ addons/ggt-debug-shortcuts/ggt-debug-shortcuts.gd
@@ -5,7 +5,7 @@ extends EditorPlugin
 # this will be loaded only on the first plugin activation. If you want to
 # change the KEYS than use Project -> Project Settings -> Input Map
 const default_debug_shortcuts = {
-	"input/debug_restart_game": KEY_R,
+	"input/debug_restart_scene": KEY_R,
 	"input/debug_pause_game": KEY_P,
 	"input/debug_quit_game": KEY_Q
 }
